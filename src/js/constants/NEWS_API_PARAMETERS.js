fce809f5d9347f576244542b2fba071aa075dada
Shestakova Elena <el.shestakova83@gmail.com>
2020-10-16 18:43:34 +0700

    change url for newsapi

--- src/js/constants/NEWS_API_PARAMETERS.js
+++ src/js/constants/NEWS_API_PARAMETERS.js
@@ -1,6 +1,6 @@
 const NEWS_API_PARAMETERS = {
-  //url: `https://nomoreparties.co/news/v2/`,
-  url: `https://newsapi.org/v2/`,
+  url: `https://nomoreparties.co/news/v2/`,
+  //url: `https://newsapi.org/v2/`,
   pageSize: '100',
   sortBy: `popularity`,
   apiKey: `9e4708b9ca334b159530dac929d9a34b`
